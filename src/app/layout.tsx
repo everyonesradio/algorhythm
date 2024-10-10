419c0ecb88406ee60eb4563daf596a366bbb6c21
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-10 19:49:58 -0400

    Migrate from pages to app router

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Algorhythm",
+  description: "AI image recognition app for vinyls",
+  icons: {
+    icon: '/',
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <div className="flex w-full max-w-5xl items-center justify-center font-mono text-sm lg:flex">
+        <h1 className="text-3xl font-bold underline">algoRhythm: AI image recognition app for vinyls</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
