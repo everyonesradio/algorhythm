24ebecc7cc4195c98bda22ff764e72e032c7902b
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-18 21:55:31 -0400

    Real-time object detection w/ TensorFlow and COCO-SSD

--- src/pages/demo/index.tsx
+++ src/pages/demo/index.tsx
@@ -1,20 +1,81 @@
 // ** React/Next.js Imports
-import React from "react";
+import React, { useRef, useEffect } from "react";
+
+// ** Third-Party Imports
+import * as tf from "@tensorflow/tfjs";
+import * as cocossd from "@tensorflow-models/coco-ssd";
+import Webcam from "react-webcam";
+
+// ** Custom Components, Hooks, Utils, etc.
+import { drawRect } from "@/utils/draw-rectangle";
 
 const Demo = () => {
-  const handleCamera = () => {
-    // TODO: Implement camera functionality
-    console.log("Camera functionality goes here");
+  const webcamRef = useRef<Webcam | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  // Main function
+  const runCocoModel = async () => {
+    try {
+      tf.backend();
+      const net = await cocossd.load();
+
+      const detectFrame = async () => {
+        await detect(net);
+        requestAnimationFrame(detectFrame);
+      };
+
+      requestAnimationFrame(detectFrame);
+    } catch (error) {
+      console.error("Error loading COCO-SSD model:", error);
+    }
   };
 
+  const detect = async (net: cocossd.ObjectDetection) => {
+    // Check data is available
+    if (
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video !== null &&
+      webcamRef.current.video.readyState === 4 &&
+      canvasRef.current !== null
+    ) {
+      // Get Video Properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // Set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // Set canvas height and width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // Make Detections
+      const obj = await net.detect(video);
+
+      // Draw mesh
+      const ctx = canvasRef.current.getContext("2d");
+      drawRect(obj, ctx!);
+    }
+  };
+
+  useEffect(() => {
+    runCocoModel();
+  }, []);
+
   return (
-    <div className='flex min-h-screen items-center justify-center p-8'>
-      <button
-        onClick={handleCamera}
-        className='flex h-10 shrink-0 items-center justify-center gap-2 text-white bg-black px-4 py-2 text-sm font-semibold transition-all'
-      >
-        <span>Scan your vinyl</span>
-      </button>
+    <div className='flex min-h-screen items-center justify-center'>
+      <Webcam
+        ref={webcamRef}
+        muted={true}
+        className='absolute left-0 right-0 mx-auto text-center z-10 w-[640px] h-[480px]'
+      />
+      <canvas
+        ref={canvasRef}
+        className='absolute left-0 right-0 mx-auto text-center z-10 w-[640px] h-[480px]'
+      />
     </div>
   );
 };
