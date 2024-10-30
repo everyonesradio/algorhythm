275417bc0de1b7bc547efda22c48049f68e50899
DOGHOUSETV <arubigeorge0@gmail.com>
2024-10-29 20:53:06 -0400

    Reduce banner text speed

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -9,7 +9,7 @@ import { PiStarFourFill } from "react-icons/pi";
 
 const Banner = () => {
   return (
-    <Marquee className='bg-black py-2' speed={100} autoFill={true}>
+    <Marquee className='bg-black py-2' speed={75} autoFill={true}>
       <div className='flex items-center gap-4 pr-4'>
         <span className='text-md text-white font-eightbitdragon'>
           Sign up for the waitlist to access our product demo!
