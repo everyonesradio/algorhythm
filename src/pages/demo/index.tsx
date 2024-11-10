a4f40c97c3925fbaa70114adba7342cd836e7c19
DOGHOUSETV <arubigeorge0@gmail.com>
2024-11-10 09:21:02 -0500

    Demo page for socials

---
+++ src/pages/demo/index.tsx
@@ -0,0 +1,36 @@
+// ** React/Next.js Imports
+import Image from "next/image";
+import React from "react";
+
+// ** Third-Party Imports
+import { TypeAnimation } from "react-type-animation";
+
+const Demo = () => {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center p-8'>
+      <div className='flex flex-col w-full max-w-5xl items-center justify-center font-sans'>
+        <Image
+          src={"/images/vinyl.png"}
+          alt={"vinyl animation"}
+          className='pb-2 animate-spin'
+          width={220}
+          height={220}
+        />
+        <TypeAnimation
+          sequence={["algoRhythm"]}
+          speed={{ type: "keyStrokeDelayInMs", value: 120 }}
+          wrapper='span'
+          cursor={false}
+          style={{
+            fontSize: "2em",
+            display: "inline-block",
+            fontFamily: "eightbitdragon",
+          }}
+        />
+        <span className='text-md font-semibold'>vinyl recognition app</span>
+      </div>
+    </main>
+  );
+};
+
+export default Demo;
