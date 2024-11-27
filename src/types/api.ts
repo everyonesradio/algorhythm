0bbfbae7127808df9274a25fbf733c46872ab83d
DOGHOUSETV <arubigeorge0@gmail.com>
2024-11-26 22:37:59 -0500

    Add musicIDX router type definitions

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,7 @@
 import type { RouterInputs, RouterOutputs } from "@/utils/trpc";
 
 export type WaitlistRouterInputs = RouterInputs["waitlist"];
+export type musicIDRouterInputs = RouterInputs["musicIDX"];
 
 export type WaitlistRouterOutputs = RouterOutputs["waitlist"];
+export type musicIDRouterOutputs = RouterOutputs["musicIDX"];
