775e1f30bf036857cf40266e05d960df2083260b
DOGHOUSETV <arubigeorge0@gmail.com>
2024-11-29 23:47:07 -0500

    Add utility functions for URL handling and link generation

---
+++ src/utils/compare.ts
@@ -0,0 +1,41 @@
+import type { AnyNode, Cheerio, CheerioAPI } from "cheerio";
+import { compareTwoStrings } from "string-similarity";
+
+import { RESPONSE_COMPARE_MIN_SCORE } from "@/config/constants";
+
+export function responseMatchesQuery(response: string, query: string) {
+  return (
+    compareTwoStrings(response.toLowerCase(), query.toLowerCase()) >
+    RESPONSE_COMPARE_MIN_SCORE
+  );
+}
+
+export function getResultWithBestScore(
+  doc: CheerioAPI,
+  listElements: Cheerio<AnyNode>,
+  query: string
+) {
+  const parsedQuery = query.toLowerCase().trim();
+
+  const resultWithBestScore = {
+    href: "",
+    score: 0,
+  };
+
+  listElements.each((_, el) => {
+    const title = doc(el).text().trim();
+    const href = doc(el).attr("href");
+    const score = compareTwoStrings(title.toLowerCase(), parsedQuery);
+
+    if (href && score > resultWithBestScore.score) {
+      resultWithBestScore.href = href;
+      resultWithBestScore.score = score;
+    }
+  });
+
+  if (resultWithBestScore.href === "") {
+    throw new Error(`Result with best score not found for: ${query}`);
+  }
+
+  return resultWithBestScore;
+}
---
+++ src/utils/url-shortener.ts
@@ -0,0 +1,49 @@
+import { ENV } from "@/config/env";
+
+import {
+  cacheShortenLink,
+  getCachedShortenLink,
+} from "@/server/services/cache";
+
+import HttpClient from "./http-client";
+import { logger } from "./logger";
+
+interface ApiResponse {
+  data: {
+    id: string;
+    refer: string;
+    origin: string;
+  };
+}
+
+export async function shortenLink(link: string) {
+  const cache = await getCachedShortenLink(link);
+  if (cache) {
+    logger.info(`[url-shortener] (${link}) cache hit`);
+    return cache;
+  }
+
+  let refer = link;
+
+  try {
+    const response = await HttpClient.post<ApiResponse>(
+      ENV.utils.urlShortener.apiUrl,
+      {
+        url: link,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          "X-Api-Key": ENV.utils.urlShortener.apiKey,
+        },
+      }
+    );
+
+    refer = response.data.refer;
+    await cacheShortenLink(link, refer);
+  } catch (err) {
+    logger.error(`[url-shortener] (${link}) request failed ${err}`);
+  }
+
+  return refer;
+}
