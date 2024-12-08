1262a818b08fd8bfa0307b50597fe46acf8cf0d2
DOGHOUSETV <arubigeorge0@gmail.com>
2024-12-08 13:11:51 -0500

    Update search results styling

--- src/components/search-card/index.tsx
+++ src/components/search-card/index.tsx
@@ -6,39 +6,39 @@ import { SearchResult } from "@/server/services/search";
 const SEARCH_LINK_DICT = {
   [Adapter.Spotify]: {
     icon: "fab fa-spotify",
-    label: "Listen on Spotify",
+    label: "Spotify",
   },
   [Adapter.YouTube]: {
     icon: "fab fa-youtube",
-    label: "Listen on YouTube Music",
+    label: "YouTube Music",
   },
   [Adapter.Deezer]: {
     icon: "fab fa-deezer",
-    label: "Listen on Deezer",
+    label: "Deezer",
   },
   [Adapter.AppleMusic]: {
     icon: "fab fa-apple",
-    label: "Listen on Apple Music",
+    label: "Apple Music",
   },
   [Adapter.Tidal]: {
     icon: "fa fa-music",
-    label: "Listen on Tidal",
+    label: "Tidal",
   },
   [Adapter.SoundCloud]: {
     icon: "fab fa-soundcloud",
-    label: "Listen on SoundCloud",
+    label: "SoundCloud",
   },
 };
 
 const SearchCard = (props: { searchResult: SearchResult }) => {
   return (
-    <div>
+    <div className='space-y-2 border-2'>
       {props.searchResult.links.map(({ type, url }) => {
         const platformInfo = SEARCH_LINK_DICT[type];
         return (
           <button
             key={type}
-            className='flex items-center space-x-2 bg-black text-white p-2 px-4 rounded-full'
+            className='flex items-center w-full space-x-2 bg-black text-white p-2 px-4 rounded-full'
             onClick={() => window.open(url, "_blank")}
           >
             <i className={`${platformInfo.icon} text-lg`} />
